380d89f47a7aa35b2acc730217f4d17811eceec6
Badreddine Ibril <badreddine.ibril@youcan.shop>
2024-03-08 10:07:59 +0100

    project setup

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Hello, Do3bol</h1>
+  </div>
+</template>
+
+<style>
+html {
+  background-color: white;
+  font: var(--text-sm-regular);
+}
+</style>
